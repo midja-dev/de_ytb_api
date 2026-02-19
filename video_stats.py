12988e79bd25839a4b0b640ec7ac19d91a5a5ff9
bertini <bertini@homesh-gbyt>
2026-02-19 12:47:25 +0100

    Update video_stats.py to add save_to_json function

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 
 load_dotenv(dotenv_path="./.env")
@@ -123,9 +124,16 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/ytb_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     print(playlistId)
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
     
